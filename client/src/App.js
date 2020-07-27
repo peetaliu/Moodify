e9ace1b463a47b022e5bd4f4a83723246f874450
Peter <peterdotliu@gmail.com>
2020-07-27 19:09:11 -0400

    Add authentication via reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
-import spotifyService from './services/spotifyService'
+import { authenticate } from './reducers/authReducer'
 import Selectors from './components/Selectors'
 
 const App = () => {
   const dispatch = useDispatch()
-  useEffect(() => {
-    spotifyService.auth()
-  })
 
+  useEffect(() => {
+    dispatch(authenticate())
+  }, [dispatch])
   return (
     <div>
       <h1>Find your mood</h1>

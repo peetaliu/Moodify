59b56d6f490ce77df6e309e2e022d79c988f83ee
Peter <peterdotliu@gmail.com>
2020-07-27 19:15:47 -0400

    Add jwt functionality for authentication token

--- server/controllers/spotify.js
+++ server/controllers/spotify.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const qs = require('querystring')
 const { Base64 } = require('js-base64')
+const jwt = require('jsonwebtoken')
 const spotifyRouter = require('express').Router()
 const config = require('../utils/config')
 const recURL = 'https://api.spotify.com/v1/recommendations'
@@ -18,7 +19,24 @@ spotifyRouter.get('/auth', async (req, res) => {
     data: qs.stringify({ grant_type: 'client_credentials' }),
   }
   const success = await axios(options)
-  console.log('res data', success.data)
+  const token = jwt.sign(success.data.access_token, process.env.SPOTIFY_SECRET)
+  // const decodedToken = jwt.verify(token, process.env.SPOTIFY_SECRET)
+  // console.log('suc obj: ', success.data.access_token)
+  // console.log('token: ', token)
+  // console.log('decoded: ', decodedToken)
+  res.json({ tok: token })
+})
+
+spotifyRouter.post('/recs', async (req, res) => {
+  const moodObj = req.body
+
+  const options = {
+    method: 'POST',
+    url: recURL,
+    headers: {
+      Authorization: `Bearer ${moodObj.tok}`,
+    },
+  }
 })
 
 module.exports = spotifyRouter

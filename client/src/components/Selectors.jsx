85b19785f6973f1bbe18979f98d3bc2b74ae2708
Peter <peterdotliu@gmail.com>
2020-07-27 19:10:24 -0400

    Implement basic redux functionality

--- client/src/components/Selectors.jsx
+++ client/src/components/Selectors.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Slider } from '@material-ui/core'
+import { createRecs } from '../reducers/spotifyReducer'
 
 const Selectors = props => {
-  return <Slider defaultValue={5} step={1} min={0} max={10} />
+  return (
+    <div>
+      <Slider defaultValue={5} step={1} min={0} max={10} />
+      <Slider defaultValue={5} step={1} min={0} max={10} />
+      <Slider defaultValue={5} step={1} min={0} max={10} />
+      <Slider defaultValue={5} step={1} min={0} max={10} />
+    </div>
+  )
 }
 
-export default Selectors
+const mapDispatchToProps = {
+  createRecs,
+}
+export default connect(null, { mapDispatchToProps })(Selectors)

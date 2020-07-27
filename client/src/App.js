393dbf9045cc09f5d0f653df15646a77812d6ff6
Peter <peterdotliu@gmail.com>
2020-07-27 01:46:44 -0400

    Add authentication, temporary display

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import spotifyService from './services/spotifyService'
+import Selectors from './components/Selectors'
 
-const App = () => {}
+const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    spotifyService.auth()
+  })
+
+  return (
+    <div>
+      <h1>Find your mood</h1>
+      <Selectors />
+    </div>
+  )
+}
 
 export default App

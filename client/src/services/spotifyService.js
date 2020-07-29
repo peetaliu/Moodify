4f4652b83a62376cd8dacd4740ae47fa6635cb4a
Peter <peterdotliu@gmail.com>
2020-07-29 19:22:27 -0400

    Add route for getting genres

--- client/src/services/spotifyService.js
+++ client/src/services/spotifyService.js
@@ -3,14 +3,17 @@ const baseURL = 'http://localhost:3001/api/spotify'
 
 const auth = async () => {
   const req = await axios.get(`${baseURL}/auth`)
-  console.log(req.data)
+  return req.data
+}
+
+const getGenres = async obj => {
+  const req = await axios.post(`${baseURL}/genres`, obj)
   return req.data
 }
 
 const getRecs = async moodObj => {
   const req = await axios.post(`${baseURL}/recs`, moodObj)
-  console.log('Recommendations: ', req.data)
   return req.data
 }
 
-export default { auth, getRecs }
+export default { auth, getRecs, getGenres }

66dacd70e702d78d49e2324fd3f636fd893c2986
Peter <peterdotliu@gmail.com>
2020-07-28 22:48:51 -0400

    Implement api call for getting track recommendations

--- server/controllers/spotify.js
+++ server/controllers/spotify.js
@@ -6,6 +6,7 @@ const spotifyRouter = require('express').Router()
 const config = require('../utils/config')
 const recURL = 'https://api.spotify.com/v1/recommendations'
 const authURL = 'https://accounts.spotify.com/api/token'
+require('express-async-errors')
 
 spotifyRouter.get('/auth', async (req, res) => {
   const authStr = Base64.encode(`${config.SPOTIFY_ID}:${config.SPOTIFY_SECRET}`)
@@ -29,14 +30,29 @@ spotifyRouter.get('/auth', async (req, res) => {
 
 spotifyRouter.post('/recs', async (req, res) => {
   const moodObj = req.body
-
+  const convMood = {
+    ac: `target_acousticness=${
+      (moodObj.energy / 10 + moodObj.aggression / 10) / 2
+    }`,
+    dnc: `target_danceability=${
+      (moodObj.energy / 10 + moodObj.happy / 10) / 2
+    }`,
+    enr: `target_energy=${moodObj.energy / 10}`,
+    ld: `target_loudness=${moodObj.aggression / 10}`,
+    va: `target_valence=${moodObj.happy / 10}`,
+  }
+  const convTok = jwt.verify(moodObj.token, process.env.SPOTIFY_SECRET)
+  console.log(convMood)
   const options = {
-    method: 'POST',
-    url: recURL,
+    method: 'GET',
+    url: `${recURL}?seed_genres=pop&limit=20&${convMood.ac}&${convMood.dnc}&${convMood.enr}&${convMood.va}`,
     headers: {
-      Authorization: `Bearer ${moodObj.tok}`,
+      Authorization: `Bearer ${convTok}`,
     },
   }
+  console.log(options.url)
+  const recs = await axios(options)
+  res.json(recs.data)
 })
 
 module.exports = spotifyRouter

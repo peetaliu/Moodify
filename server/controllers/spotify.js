9b1519aa0a0b69b64ef59d4f6ebd59282bfe8078
Peter <peterdotliu@gmail.com>
2020-07-29 19:26:57 -0400

    Rework get recommendations route, Add genre route

--- server/controllers/spotify.js
+++ server/controllers/spotify.js
@@ -8,6 +8,7 @@ const recURL = 'https://api.spotify.com/v1/recommendations'
 const authURL = 'https://accounts.spotify.com/api/token'
 require('express-async-errors')
 
+//Authentication
 spotifyRouter.get('/auth', async (req, res) => {
   const authStr = Base64.encode(`${config.SPOTIFY_ID}:${config.SPOTIFY_SECRET}`)
   const options = {
@@ -21,36 +22,40 @@ spotifyRouter.get('/auth', async (req, res) => {
   }
   const success = await axios(options)
   const token = jwt.sign(success.data.access_token, process.env.SPOTIFY_SECRET)
-  // const decodedToken = jwt.verify(token, process.env.SPOTIFY_SECRET)
-  // console.log('suc obj: ', success.data.access_token)
-  // console.log('token: ', token)
-  // console.log('decoded: ', decodedToken)
   res.json({ tok: token })
 })
 
+//Get genres
+spotifyRouter.post('/genres', async (req, res) => {
+  const token = req.body
+  const convTok = jwt.verify(token, process.env.SPOTIFY_SECRET)
+  const genres = await axios.get(`${recURL}/available-genre-seeds`, {
+    headers: { Authorization: `Bearer ${convTok}` },
+  })
+})
+
+//Get recommendations
 spotifyRouter.post('/recs', async (req, res) => {
   const moodObj = req.body
-  const convMood = {
-    ac: `target_acousticness=${
-      (moodObj.energy / 10 + moodObj.aggression / 10) / 2
-    }`,
-    dnc: `target_danceability=${
-      (moodObj.energy / 10 + moodObj.happy / 10) / 2
-    }`,
-    enr: `target_energy=${moodObj.energy / 10}`,
-    ld: `target_loudness=${moodObj.aggression / 10}`,
-    va: `target_valence=${moodObj.happy / 10}`,
+  const params = {
+    seed_genres: moodObj.genres.join(),
+    limit: '20',
+    target_acousticness: (moodObj.energy / 10 + moodObj.aggression / 10) / 2,
+    target_danceability: (moodObj.energy / 10 + moodObj.happy / 10) / 2,
+    target_energy: moodObj.energy / 10,
+    target_loudness: moodObj.aggression / 10,
+    target_valence: moodObj.happy / 10,
   }
   const convTok = jwt.verify(moodObj.token, process.env.SPOTIFY_SECRET)
-  console.log(convMood)
+
   const options = {
     method: 'GET',
-    url: `${recURL}?seed_genres=pop&limit=20&${convMood.ac}&${convMood.dnc}&${convMood.enr}&${convMood.va}`,
+    url: `${recURL}`,
+    params: params,
     headers: {
       Authorization: `Bearer ${convTok}`,
     },
   }
-  console.log(options.url)
   const recs = await axios(options)
   res.json(recs.data)
 })

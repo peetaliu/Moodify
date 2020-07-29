042651407f4ece1325b00efb8bc33a3bdb0270ac
Peter <peterdotliu@gmail.com>
2020-07-28 22:44:38 -0400

    Implement working api calls

--- client/src/components/Selectors.jsx
+++ client/src/components/Selectors.jsx
@@ -1,15 +1,72 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Slider } from '@material-ui/core'
+import React, { useState } from 'react'
+import { connect, useSelector, useDispatch } from 'react-redux'
+import { Slider, Typography } from '@material-ui/core'
 import { createRecs } from '../reducers/spotifyReducer'
 
 const Selectors = props => {
+  const [happiness, setHappiness] = useState(5)
+  const [energy, setEnergy] = useState(5)
+  const [aggression, setAggression] = useState(5)
+  const token = useSelector(state => state.auth)
+  const tracks = useSelector(state => state.spotify)
+  const dispatch = useDispatch()
+
+  const handleSubmit = () => {
+    const moodObj = {
+      happy: happiness,
+      energy: energy,
+      aggression: aggression,
+      token: token,
+    }
+    dispatch(createRecs(moodObj))
+    console.log(tracks)
+  }
+
   return (
     <div>
-      <Slider defaultValue={5} step={1} min={0} max={10} />
-      <Slider defaultValue={5} step={1} min={0} max={10} />
-      <Slider defaultValue={5} step={1} min={0} max={10} />
-      <Slider defaultValue={5} step={1} min={0} max={10} />
+      <Typography>Happiness</Typography>
+      <Slider
+        defaultValue={5}
+        step={1}
+        min={0}
+        max={10}
+        marks={[
+          { value: 0, label: 'Sad' },
+          { value: 10, label: 'Happy' },
+        ]}
+        onChange={(e, value) => {
+          setHappiness(value)
+        }}
+      />
+      <Typography>Energy</Typography>
+      <Slider
+        defaultValue={5}
+        step={1}
+        min={0}
+        max={10}
+        marks={[
+          { value: 0, label: 'Calm' },
+          { value: 10, label: 'Energetic' },
+        ]}
+        onChange={(e, value) => {
+          setEnergy(value)
+        }}
+      />
+      <Typography>Aggression</Typography>
+      <Slider
+        defaultValue={5}
+        step={1}
+        min={0}
+        max={10}
+        marks={[
+          { value: 0, label: 'Peaceful' },
+          { value: 10, label: 'Aggressive' },
+        ]}
+        onChange={(e, value) => {
+          setAggression(value)
+        }}
+      />
+      <button onClick={handleSubmit}>Get Playlist</button>
     </div>
   )
 }

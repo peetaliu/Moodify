dea757ae9349c92457e2661b7d808935ee5eb03a
Peter <peterdotliu@gmail.com>
2020-07-29 19:24:08 -0400

    Add genre reducer to store

--- client/src/store.js
+++ client/src/store.js
@@ -2,12 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
-import spotifyReducer from './reducers/spotifyReducer'
+import recsReducer from './reducers/recsReducer'
 import authReducer from './reducers/authReducer'
+import genreReducer from './reducers/genreReducer'
 
 const reducer = combineReducers({
-  spotify: spotifyReducer,
+  recs: recsReducer,
   auth: authReducer,
+  genres: genreReducer,
 })
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))

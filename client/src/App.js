e06199c1bb1b688fdad938bf225aad48355abc38
Peter <peterdotliu@gmail.com>
2020-08-06 22:30:30 -0400

    Update useEffect hook, Import TrackList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import './App.css'
 import { authenticate } from './reducers/authReducer'
+import { getPop } from './reducers/genreReducer'
 import Selectors from './components/Selectors'
+import TrackList from './components/TrackList'
 
 const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(authenticate())
+    dispatch(getPop())
   }, [dispatch])
+
   return (
     <div>
       <h1>Find your mood</h1>
       <Selectors />
+      <TrackList />
     </div>
   )
 }

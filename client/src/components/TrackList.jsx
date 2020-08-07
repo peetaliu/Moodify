dc41343a783e4ea75abdf65b6f969ca5fa4733f9
Peter <peterdotliu@gmail.com>
2020-08-06 22:35:06 -0400

    Add display list of recs

---
+++ client/src/components/TrackList.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Container, Row, Col, Card } from 'react-bootstrap'
+
+const TrackList = props => {
+  return (
+    <div id='trackList'>
+      <Container>
+        <Row>
+          {props.recs.tracks &&
+            props.recs.tracks.map(t => (
+              <Col key={t.id}>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>{t.name}</Card.Title>
+                    {t.artists.map(a => (
+                      <Card.Subtitle key={a.id}>{a.name}</Card.Subtitle>
+                    ))}
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </Container>
+      {console.log(props.recs.tracks)}
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    recs: state.recs,
+  }
+}
+
+export default connect(mapStateToProps)(TrackList)

81099f665a7f7453673598ca186d2a831a9788ea
Peter <peterdotliu@gmail.com>
2020-08-07 23:44:53 -0400

    Remove app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import './App.css'
 import { authenticate } from './reducers/authReducer'
 import { getPop } from './reducers/genreReducer'
 import Selectors from './components/Selectors'
